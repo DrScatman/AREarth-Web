92f96cf604a0cb8224619e253ad9e073645f491d
radderic <darre@mail.gvsu.edu>
2020-03-13 16:38:59 -0400

    added general UI for login with firebase authentication

--- final/src/components/Login.vue
+++ final/src/components/Login.vue
@@ -15,28 +15,52 @@
                     <v-col cols="8" class="text-center">
                         <v-card>
                             <v-toolbar>
-                                <v-toolbar-title>Login/Signup</v-toolbar-title>
+                                <v-toolbar-title>Login</v-toolbar-title>
                             </v-toolbar>
                             <v-container class="pa-8">
                                 <v-card-text>
                                     <v-form>
-                                        <v-text-field label="Login" v-model="email"></v-text-field>
+                                        <v-text-field label="GVSU Email" v-model="email" placeholder="user@gvsu.edu"></v-text-field>
                                         <v-text-field label="Password" v-model="password" type="password"></v-text-field>
                                     </v-form>
                                 </v-card-text>
                                 <v-card-actions>
                                     <v-spacer />
-                                        <v-btn color="blue">Login</v-btn>
+                                        <v-btn v-if="!isLoggedIn" @click="login" color="blue" class="px-8" :disabled="!email || !password" required>Login</v-btn>
+                                        <v-btn v-if="isLoggedIn" @click="logout" color="blue" required>Signout</v-btn>
                                     <v-spacer />
                                     </v-card-actions>
                                 </v-container>
                                 <v-divider />
                                 <v-card-text>
-                                    <v-btn color="green">Create new account</v-btn>
+                                    <v-btn @click="createDialog = true" color="green">Create new account</v-btn>
+                                    <v-dialog v-model="createDialog">
+                                        <v-card>
+                                            <v-toolbar>
+                                                <v-toolbar-title>Create new account</v-toolbar-title>
+                                            </v-toolbar>
+                                            <v-container class="pa-8">
+                                                <v-card-text>
+                                                    <v-form>
+                                                        <v-text-field label="First name" v-model="firstName" required></v-text-field>
+                                                        <v-text-field label="Last name" v-model="lastName" required></v-text-field>
+                                                        <v-text-field label="GVSU Email" v-model="newAccountEmail" placeholder="user@gvsu.edu" required></v-text-field>
+                                                        <v-text-field label="Password" v-model="newAccountPassword" type="password"></v-text-field>
+                                                        <v-text-field label="Confirm Password" v-model="confirmPassword" type="password"></v-text-field>
+                                                    </v-form>
+                                                </v-card-text>
+                                                <v-card-actions>
+                                                    <v-btn @click="createDialog = false" color="red">Cancel</v-btn>
+                                                    <v-spacer />
+                                                    <v-btn @click="createAccount" color="green">Create Account</v-btn>
+                                                </v-card-actions>
+                                            </v-container>
+                                        </v-card>
+                                    </v-dialog>
                                 </v-card-text>
                                 <v-divider />
                                 <v-card-text>
-                                    <v-btn color="yellow" class="black--text">Forgot my password</v-btn>
+                                    <v-btn @click="forgotPassword" color="yellow" class="black--text">Forgot my password</v-btn>
                                 </v-card-text>
                         </v-card>
                     </v-col>
@@ -48,13 +72,77 @@
 
 
 <script>
+import { AppDB, AppAuth } from './db-init.js'
+
 export default {
     name: 'Login',
-    components: {
-    },
     data: () => ({
         email: "",
         password: "",
+        newAccountEmail: "",
+        newAccountPassword: "",
+        confirmPassword: "",
+        firstName: "",
+        lastName: "",
+
+        isLoggedIn: false,
+        createDialog: false,
+
+        emailRules: [
+            v => !!v || 'Email is required',
+            v => (v.endsWith("@gvsu.edu")) || 'Must be a valid gvsu email',
+        ],
+
+        nameRules: [
+            v => !!v || 'Name is required',
+        ],
+
     }),
+    methods: {
+        login() {
+            AppAuth.signInWithEmailAndPassword(this.email, this.password)
+                .then((u) => {
+                    console.log("You logged in as " + u.user.email);
+                })
+                .catch((err) => {
+                    console.log("Error: " + err.message);
+                });
+        },
+        createAccount() {
+            AppAuth.createUserWithEmailAndPassword(this.newAccountEmail, this.newAccountPassword)
+                .then((u) => {
+                    console.log("User created with UID " + u.user.uid);
+                    AppDB.ref('users/' + u.user.uid).set({
+                        firstName: this.firstName, 
+                        lastName: this.lastName,
+                    })
+                    this.createDialog = false
+                })
+                .catch((err) => {
+                    console.log("Error: " + err.message);
+                });
+        },
+        forgotPassword() {
+            console.log("Send email to reset password")
+        },
+        logout() {
+            AppAuth.signOut().then(() => {
+                console.log("Signed out");
+            });
+        },
+    },
+    mounted() {
+        AppAuth.onAuthStateChanged((u) => {
+            if (u == null) {
+                console.log("not logged in")
+                this.isLoggedIn = false
+            }
+            else {
+                console.log("is logged in as: " + u.uid)
+                this.isLoggedIn = true
+                //go to the next page automatically
+            }
+        });
+    },
 }
 </script>
