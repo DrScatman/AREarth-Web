998c40044758cc12fab760a8578cf8d4be0a36f4
Jake Miller <jacob.miller@initechglobal.com>
2020-06-05 15:16:56 -0400

    Initial commit

--- prototype/src/models.js
+++
@@ -1,9 +0,0 @@
-import helmet from './models/DamagedHelmet.glb';
-import fox from './models/Fox.glb';
-import duck from './models/Duck.glb';
-import robot from './models/BrainStem.glb';
-
-import pyramid from './models/pyramid.obj';
-import pymtl from './models/pyramid.mtl';
-
-export { helmet, fox, duck, robot, pyramid, pymtl }
